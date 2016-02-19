01790fa1cd967fc957f30b23d22a649bede51439
Mert Inan <Mert.Inan@thomsonreuters.com>
2016-02-19 11:13:27 +0000

    init

--- src/main/java/example/SpringAsyncDemoApplication.java
+++ src/main/java/example/SpringAsyncDemoApplication.java
@@ -1,7 +1,5 @@
 package example;
 
-import java.util.stream.LongStream;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/example/controller/AsyncController.java
+++ src/main/java/example/controller/AsyncController.java
@@ -1,51 +1,62 @@
 package example.controller;
 
-import java.util.*;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/rest")
 public class AsyncController {
 
-    /**
-     * Hello world, asynchronously
-     *
-     * @return a promise (deferred) of the result
-     */
-    @RequestMapping({"/hello"})
-    public DeferredResult<Map<String, String>> hello() {
-        DeferredResult<Map<String, String>> deferred = new DeferredResult<>();
-
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Map<String, String> result = new HashMap<>();
-                result.put("hello", "WORLD");
-                deferred.setResult(result);
+    @RequestMapping({"/async"})
+    public DeferredResult<Long> async() {
+        DeferredResult<Long> deferred = new DeferredResult<>();
+        long start = System.currentTimeMillis();
+        CompletableFuture<Long> result = CompletableFuture.supplyAsync(() -> new Service(2l).call());
+        result.whenComplete((stamp, ex) -> {
+            if (ex != null) {
+                deferred.setErrorResult(ex);
+            } else {
+                deferred.setResult(stamp - start);
             }
-        }, Math.round(Math.random() * 200 + 1900));
-
+        });
         return deferred;
     }
 
-    /**
-     * Traditional hello world, blocking.
-     *
-     * @return json object of the result
-     */
-    @RequestMapping({"/blocking-hello"})
-    public Map<String, String> blockingHello() {
-        Map<String, String> result = new HashMap<>();
+    @RequestMapping({"/sync"})
+    public Long sync() {
+        long start = System.currentTimeMillis();
         try {
             Thread.sleep(Math.round(Math.random() * 200 + 1900));
-            result.put("hello", "blocking");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return result;
+        return System.currentTimeMillis() - start;
     }
 }
+
+
+class Service implements Callable<Long> {
+
+    private final Long value;
+
+    public Service(Long value) {
+        this.value = value;
+    }
+
+    @Override
+    public Long call() {
+        try {
+            TimeUnit.SECONDS.sleep(value);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return System.currentTimeMillis();
+    }
+}
+
+
